e7486df7fd0fac8bc170d4e443901ba5ac8e8de9
Jordi <llorcajor22@gmail.com>
2022-04-30 13:18:55 +0200

    ssr user profile page

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,7 +1,36 @@
-const UserProfilePage = () => {
+import PostFeed from "../../components/PostFeed";
+import UserProfile from "../../components/UserProfile";
+import { getUserWithUsername, postToJSON } from "../../lib/firebase";
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .limit(5);
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
+}
+
+const UserProfilePage = ({ user, posts }) => {
   return (
     <main>
-      <h1>Sign Up</h1>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} admin />
     </main>
   );
 };
